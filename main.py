e3fac0fe7d82325ab5917f140ad3972791ae340b
ericsiranda <ericsiranda@gmail.com>
2026-09-14 18:13:14 +0700

    Kembali ke Library Lama

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ from vercel_blob import generate_client_token
 
 app = Flask(__name__)
 
-# Route untuk halaman utama (menampilkan form upload)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Route API untuk membuat token upload ke Vercel Blob
 @app.route('/api/upload-token', methods=['POST'])
 def upload_token():
     try:
-        # Membuat token yang hanya berlaku untuk 1x upload, maksimal 500MB, khusus video
         token = generate_client_token(
             allowed_content_types=['video/*'],
-            maximum_size_in_bytes=500 * 1024 * 1024, # 500 MB
+            maximum_size_in_bytes=500 * 1024 * 1024,
         )
         return jsonify({'token': token})
     except Exception as e:
